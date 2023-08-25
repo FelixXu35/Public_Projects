e3ced309a350642dba9336d39f5a3f6cdcf25539
Felix Xu <61252303+FelixXu35@users.noreply.github.com>
2023-08-25 16:39:38 +0100

    integer QAOA begins to work

--- projects/5_integer_QAOA/supply.py
+++ projects/5_integer_QAOA/supply.py
@@ -1,4 +1,7 @@
+from typing import List
+
 import numpy as np
+import tensorcircuit as tc
 
 
 def integer_QAOA_transformation(integers: list):
@@ -10,11 +13,13 @@ def integer_QAOA_transformation(integers: list):
     the way of building transformation matrix is from
     Integer Programming from Quantum Annealing and Open Quantum Systems
     """
-    n_qubits = np.shape(integers)[0] # the dimension of the Q-matrix before transformation
+    n_qubits = np.shape(integers)[
+        0
+    ]  # the dimension of the Q-matrix before transformation
     matrix = np.array([[] for _ in integers], dtype=int)  # the transformation matrix
     for i, R in enumerate(integers):
         n = int(np.floor(np.log2(R + 1)))
-        
+
         binary_number = np.binary_repr(R - 2**n + 1, width=n)
         l = [1 for _ in range(n)]
         for index, number in enumerate(binary_number):
@@ -26,6 +31,102 @@ def integer_QAOA_transformation(integers: list):
             for _ in range(repetition):
                 single_matrix[i][position] = int(2**power)
                 position += 1
-        print(n, l, binary_number, single_matrix)
         matrix = np.concatenate((matrix, single_matrix), axis=1)
     return matrix
+
+
+def print_result_cost(
+    c: tc.Circuit, Q: List[list], wrap: bool = False, reverse: bool = False
+) -> None:
+    """
+    Print the results and costs of a given quantum circuit.
+    Specificly designed for the variational circuit.
+    The default order is from the highest probability to the lowest one.
+
+    :param c: The quantum circuit to print the results and probabilities.
+    :param Q: The n-by-n square and symmetric Q-matrix representing the QUBO problem.
+    :param wrap (optional): A flag indicating whether to wrap the output. Default is False.
+    :param reverse (optional): A flag indicating whether to reverse the order of the output. Default is False.
+    """
+    cost_dict = {}
+    states = []
+    n_qubits = c._nqubits
+    for i in range(2**n_qubits):
+        a = f"{bin(i)[2:]:0>{n_qubits}}"
+        states.append(a)
+        # Generate all possible binary states for the given number of qubits
+    for selection in states:
+        x = np.array([int(bit) for bit in selection])
+        cost_dict[selection] = np.dot(x, np.dot(Q, x))
+    cost_sorted = dict(sorted(cost_dict.items(), key=lambda item: item[1]))
+    if reverse == True:
+        cost_sorted = dict(
+            sorted(cost_dict.items(), key=lambda item: item[1], reverse=True)
+        )
+    num = 0
+    print("\n-------------------------------------")
+    print("    selection\t  |\t  cost")
+    print("-------------------------------------")
+    for k, v in cost_sorted.items():
+        print("%10s\t  |\t%.4f" % (k, v))
+        num += 1
+        if (num >= 8) & (wrap == True):
+            break
+    print("-------------------------------------")
+
+    def print_result_prob(c, wrap=False, reverse=False):
+        states = []
+        n_qubits = c._nqubits
+        for i in range(2**n_qubits):
+            a = f"{bin(i)[2:]:0>{n_qubits}}"
+            states.append(a)
+            # Generate all possible binary states for the given number of qubits
+
+        probs = K.numpy(c.probability()).round(decimals=4)
+        # Calculate the probabilities of each state using the circuit's probability method
+
+        sorted_indices = np.argsort(probs)[::-1]
+        if reverse == True:
+            sorted_indices = sorted_indices[::-1]
+        state_sorted = np.array(states)[sorted_indices]
+        prob_sorted = np.array(probs)[sorted_indices]
+        # Sort the states and probabilities in descending order based on the probabilities
+
+        print("\n-------------------------------------")
+        print("    selection\t  |\tprobability")
+        print("-------------------------------------")
+        if wrap == False:
+            for i in range(len(states)):
+                print("%10s\t  |\t  %.4f" % (state_sorted[i], prob_sorted[i]))
+                # Print the sorted states and their corresponding probabilities
+        elif wrap == True:
+            for i in range(4):
+                print("%10s\t  |\t  %.4f" % (state_sorted[i], prob_sorted[i]))
+            print("               ... ...")
+            for i in range(-5, -1):
+                print("%10s\t  |\t  %.4f" % (state_sorted[i], prob_sorted[i]))
+        print("-------------------------------------")
+
+
+def all_quantum_states(n_qubits, budget=None, vec=False) -> list:
+    states = []
+    for i in range(2**n_qubits):
+        a = f"{bin(i)[2:]:0>{n_qubits}}"
+        n_ones = 0
+        mark = True
+        if isinstance(budget, int):
+            for j in a:
+                if j == "1":
+                    n_ones += 1
+            if n_ones >= budget:
+                mark = False
+        if mark is True:
+            if vec == False:
+                states.append(a)
+            if vec == True:
+                vector = [0 for i in range(n_qubits)]
+                for i, j in enumerate(a):
+                    if j == "1":
+                        vector[i] = 1
+                states.append(vector)
+    return states
